615237979512a06e4a3bfdf2835435fa5bc1f77a
Suhani1607 <suhanisagar109@gmail.com>
2026-09-02 23:51:07 +0530

    Fix CORS for production

--- resume-ai-frontend/src/api/ResumeService.js
+++ resume-ai-frontend/src/api/ResumeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8080";
+export const baseURL = "https://resume-ai-p644.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: baseURL,
